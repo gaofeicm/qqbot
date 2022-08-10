b3de05f7a2af6c67545c72835182a33d04146b3b
Gaofeicm <gaofeicm@qq.com>
2022-08-10 22:40:11 +0800

    增加HttpDeleteWithBody类

---
+++ src/main/java/com/gaofeicm/qqbot/utils/entity/HttpDeleteWithBody.java
@@ -0,0 +1,33 @@
+package com.gaofeicm.qqbot.utils.entity;
+
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+
+import java.net.URI;
+
+/**
+ * @author Gaofeicm
+ */
+public class HttpDeleteWithBody extends HttpEntityEnclosingRequestBase {
+
+    public static final String METHOD_NAME = "DELETE";
+
+    @Override
+    public String getMethod() {
+        return METHOD_NAME;
+    }
+
+    public HttpDeleteWithBody(final String uri) {
+        super();
+        setURI(URI.create(uri));
+    }
+
+    public HttpDeleteWithBody(final URI uri) {
+        super();
+        setURI(uri);
+    }
+
+    public HttpDeleteWithBody() {
+        super();
+    }
+
+}
