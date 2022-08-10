dcb207dbec5114581fb10259507002312a34db27
Gaofeicm <gaofeicm@qq.com>
2022-08-10 22:36:55 +0800

    增加添加青龙面板等命令实现

--- src/main/java/com/gaofeicm/qqbot/command/CommandService.java
+++ src/main/java/com/gaofeicm/qqbot/command/CommandService.java
@@ -2,15 +2,23 @@ package com.gaofeicm.qqbot.command;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
+import com.gaofeicm.qqbot.command.entity.Command;
 import com.gaofeicm.qqbot.entity.Cookie;
-import com.gaofeicm.qqbot.service.CookieServiceImpl;
+import com.gaofeicm.qqbot.entity.Ql;
+import com.gaofeicm.qqbot.entity.QlCookie;
+import com.gaofeicm.qqbot.service.CookieService;
+import com.gaofeicm.qqbot.service.QlCookieService;
+import com.gaofeicm.qqbot.service.QlService;
+import com.gaofeicm.qqbot.utils.CommandUtils;
+import com.gaofeicm.qqbot.utils.CommonUtils;
 import com.gaofeicm.qqbot.utils.CookieUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author Gaofeicm
@@ -19,7 +27,15 @@ import java.util.Date;
 public class CommandService {
 
     @Resource
-    private CookieServiceImpl cookieService;
+    private CookieService cookieService;
+
+    @Resource
+    private QlService qlService;
+
+    @Resource
+    private QlCookieService qlCookieService;
+
+    private final static SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     /**
      * 设置ck一个月有效
@@ -64,7 +80,7 @@ public class CommandService {
      */
     private JSONObject setCookieExp(JSONObject param, int month){
         JSONArray message = new JSONArray();
-        Date expDate = this.subMonth(new Date(), month);
+        Date expDate = CommonUtils.subMonth(new Date(), month);
         Cookie cookie = new Cookie();
         cookie.setId(param.getString("id"));
         cookie.setAvailable(1);
@@ -80,7 +96,7 @@ public class CommandService {
             }});
             message.add(new JSONObject() {{
                 put("to", param.getString("from"));
-                put("msg", "管理员通过了您的申请！有效期:" + month +"个月,到期时间:" + expDateStr);
+                put("msg", "账号加入代挂！有效期:" + month +"个月,到期时间:" + expDateStr);
             }});
         }else{
             message.add(new JSONObject(){{
@@ -92,17 +108,187 @@ public class CommandService {
         return param;
     }
 
-    /****
-     * 传入具体日期 ，返回具体日期增加n个月。
-     * @param date 时间
-     * @param month 差值
-     * @return date
+    /**
+     * 添加青龙面板引导信息
+     * @param param 参数
+     * @return json
      */
-    private Date subMonth(Date date, int month) {
-        Calendar rightNow = Calendar.getInstance();
-        rightNow.setTime(date);
-        rightNow.add(Calendar.MONTH, month);
-        return rightNow.getTime();
+    public JSONObject addQlStr(JSONObject param){
+        JSONArray message = new JSONArray();
+        StringBuilder msg = new StringBuilder();
+        msg.append("请按下面模板完善json:\r\n");
+        msg.append("addQl{\"name\":\"haxvz\",\"address\":\"http://haxvz.gaofeicm.cf:8080\",\"clientId\":\"\",\"clientSecret\":\"\",\"weigth\":\"9\",\"maxCount\":100}");
+        Command command = CommandUtils.getDefaultCommand(message.toString());
+        command.setOption("addQl");
+        command.setAction("addQl");
+        command.putParam("from", param.getString("to")).putParam("to", param.getString("to"));
+        CommandUtils.addCommand(param.getString("to"), command);
+        message.add(new JSONObject(){{
+            put("to", param.getString("to"));
+            put("msg", msg.toString());
+        }});
+        param.put("message", message);
+        return param;
+    }
+
+    /**
+     * 添加青龙面板
+     * @param param 参数
+     * @return json
+     */
+    public JSONObject addQl(JSONObject param){
+        JSONArray message = new JSONArray();
+        StringBuilder msg = new StringBuilder();
+        String m = param.getString("msg").substring(5);
+        Ql ql = JSONObject.parseObject(m, Ql.class);
+        JSONObject qlToken = CommandUtils.getQlToken(ql.getAddress() + "/open/auth/token?client_id=" + ql.getClientId() + "&client_secret=" + ql.getClientSecret());
+        if(qlToken == null){
+            msg.append("配置有误,无法获取登录凭证!");
+        }else{
+            ql.setTokeType(qlToken.getString("token_type"));
+            ql.setToken(qlToken.getString("token"));
+            int i = qlService.saveQl(ql);
+            if(i > 0){
+                msg.append("添加成功！青龙面板的id为：" + ql.getId());
+            }else{
+                msg.append("添加失败！请查看日志！");
+            }
+        }
+        message.add(new JSONObject(){{
+            put("to", param.getString("to"));
+            put("msg", msg.toString());
+        }});
+        param.put("message", message);
+        return param;
+    }
+
+    /**
+     * 查询青龙面板
+     * @param param 参数
+     * @return json
+     */
+    public JSONObject findQl(JSONObject param){
+        JSONArray message = new JSONArray();
+        List<Ql> qls = qlService.getQl(new HashMap<>(0));
+        StringBuilder msg = new StringBuilder("----------------------------面板列表----------------------\r\n");
+        msg.append("序号  名称     地址                                                ck数量  最大数量  权重\r\n");
+        for (int i = 0; i < qls.size(); i++) {
+            Ql ql = qls.get(i);
+            msg.append(i + 1).append("       ").append(ql.getName()).append("   ").append(ql.getAddress()).append("    ").append(ql.getCookieCount()).append("         ").append(ql.getMaxCount()).append("       ").append(ql.getWeigth()).append("\r\n");
+        }
+        message.add(new JSONObject(){{
+            put("to", param.getString("to"));
+            put("msg", msg.toString());
+        }});
+        param.put("message", message);
+        return param;
+    }
+
+    /**
+     * 查询青龙面板
+     * @param param 参数
+     * @return json
+     */
+    public JSONObject findCookie(JSONObject param){
+        JSONArray message = new JSONArray();
+        List<Cookie> cookies = cookieService.getCookie(new HashMap<>(0));
+        StringBuilder msg = new StringBuilder("----------------------------cookie列表----------------------\r\n");
+        msg.append("序号  QQ                  pin                              启用    到期时间\r\n");
+        for (int i = 0; i < cookies.size(); i++) {
+            Cookie cookie = cookies.get(i);
+            msg.append(i + 1).append("      ").append(cookie.getQq()).append("    ").append(cookie.getPtPin(), 7, cookie.getPtPin().length() - 1).append("       ").append((boolean)cookie.getAvailable() ? "是" : "否").append("  ").append(SDF.format(cookie.getExpirationTime())).append("\r\n");
+        }
+        message.add(new JSONObject(){{
+            put("to", param.getString("to"));
+            put("msg", msg.toString());
+        }});
+        param.put("message", message);
+        return param;
+    }
+
+    /**
+     * 添加青龙面板引导信息
+     * @param param 参数
+     * @return json
+     */
+    public JSONObject addQlCookieStr(JSONObject param){
+        JSONArray message = new JSONArray();
+        StringBuilder msg = new StringBuilder();
+        msg.append("请按下面模板完善json:\r\n");
+        msg.append("addQlCookie{\"qlId\":\"1556920808510341122\",\"cookieId\":\"1556554834355539970\"}");
+        Command command = CommandUtils.getDefaultCommand(message.toString());
+        command.setOption("addQlCookie");
+        command.setAction("addQlCookie");
+        command.putParam("from", param.getString("to")).putParam("to", param.getString("to"));
+        CommandUtils.addCommand(param.getString("to"), command);
+        message.add(new JSONObject(){{
+            put("to", param.getString("to"));
+            put("msg", msg.toString());
+        }});
+        param.put("message", message);
+        return param;
+    }
+
+    /**
+     * 添加青龙面板
+     * @param param 参数
+     * @return json
+     */
+    public JSONObject addQlCookie(JSONObject param){
+        JSONArray message = new JSONArray();
+        StringBuilder msg = new StringBuilder();
+        String m = param.getString("msg").substring(11);
+        QlCookie qlCookie = JSONObject.parseObject(m, QlCookie.class);
+        int i = qlCookieService.saveQlCookie(qlCookie);
+        if(i > 0){
+            msg.append("添加成功！面板与账号映射的id为：" + qlCookie.getId());
+        }else{
+            msg.append("添加失败！请查看日志！");
+        }
+        message.add(new JSONObject(){{
+            put("to", param.getString("to"));
+            put("msg", msg.toString());
+        }});
+        param.put("message", message);
+        return param;
+    }
+
+    /**
+     * 设置ck过期时间
+     * @param param 参数
+     * @return json
+     */
+    public JSONObject setCookieExpStr(JSONObject param){
+        JSONArray message = new JSONArray();
+        StringBuilder msg = new StringBuilder();
+        msg.append("请按下面模板完善json:\r\n");
+        msg.append("setCookieExpDate{\"id\":\"1556554834355539970\",\"from\":\"\",\"to\":\"952219232\",\"ck\":\"\",\"month\":\"\"}");
+        Command command = CommandUtils.getDefaultCommand(message.toString());
+        command.setOption("setCookieExpDate");
+        command.setAction("setCookieExpDate");
+        command.putParam("from", param.getString("to")).putParam("to", param.getString("to"));
+        CommandUtils.addCommand(param.getString("to"), command);
+        message.add(new JSONObject(){{
+            put("to", param.getString("to"));
+            put("msg", msg.toString());
+        }});
+        param.put("message", message);
+        return param;
+    }
+
+    /**
+     * 设置ck过期时间
+     * @param param 参数
+     * @return json
+     */
+    public JSONObject setCookieExpDate(JSONObject param){
+        String m = param.getString("msg").substring(16);
+        JSONObject obj = JSONObject.parseObject(m);
+        if(obj.getIntValue("month") == 12){
+            return this.setCookieExpForYear(obj);
+        }else{
+            return this.setCookieExpForMonth(obj);
+        }
     }
 
 }
