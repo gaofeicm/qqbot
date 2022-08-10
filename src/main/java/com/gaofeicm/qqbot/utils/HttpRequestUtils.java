5d1f762b7ccccd948e961049fc5c9b841801926a
Gaofeicm <gaofeicm@qq.com>
2022-08-10 22:40:45 +0800

    调整http请求的实现方式，简化代码

--- src/main/java/com/gaofeicm/qqbot/utils/HttpRequestUtils.java
+++ src/main/java/com/gaofeicm/qqbot/utils/HttpRequestUtils.java
@@ -1,11 +1,11 @@
 package com.gaofeicm.qqbot.utils;
 
+import com.gaofeicm.qqbot.utils.entity.HttpDeleteWithBody;
+import lombok.SneakyThrows;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.*;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -20,55 +20,52 @@ public class HttpRequestUtils {
      * @Description: 发送get请求
      */
     public static String doGet(String url) {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(url);
-        httpGet.setHeader("Content-type", "application/json");
-        httpGet.setHeader("DataEncoding", "UTF-8");
-        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(35000).setConnectionRequestTimeout(35000).setSocketTimeout(60000).build();
-        httpGet.setConfig(requestConfig);
-        CloseableHttpResponse httpResponse = null;
-        try {
-            httpResponse = httpClient.execute(httpGet);
-            HttpEntity entity = httpResponse.getEntity();
-            if (httpResponse.getStatusLine().getStatusCode() != 200) {
-                return null;
-            }
-            return EntityUtils.toString(entity);
-        } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } finally {
-            if (httpResponse != null) {
-                try {
-                    httpResponse.close();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-            if (null != httpClient) {
-                try {
-                    httpClient.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return null;
+        return doRequest(new HttpGet(url), null);
     }
 
     public static String doGet(String url, Map<String, String> header) {
+        return doRequest(new HttpGet(url), header);
+    }
+
+    /**
+     * @Description: 发送http post请求
+     */
+    @SneakyThrows
+    public static String doPost(String url, String jsonStr, Map<String, String> header) {
+        HttpPost http = new HttpPost(url);
+        http.setEntity(new StringEntity(jsonStr));
+        return doRequest(http, header);
+    }
+
+    /**
+     * @Description: 发送http post请求
+     */
+    @SneakyThrows
+    public static String doPost(String url, String jsonStr) {
+        HttpPost http = new HttpPost(url);
+        http.setEntity(new StringEntity(jsonStr));
+        return doRequest(http, null);
+    }
+
+    @SneakyThrows
+    public static String doDelete(String url, String jsonStr, Map<String, String> header) {
+        HttpDeleteWithBody http = new HttpDeleteWithBody(url);
+        http.setEntity(new StringEntity(jsonStr));
+        return doRequest(http, header);
+    }
+
+    public static String doRequest(HttpRequestBase http, Map<String, String> header) {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(url);
-        header.forEach((k,v) -> httpGet.setHeader(k, v));
+        http.setHeader("Content-type", "application/json");
+        http.setHeader("DataEncoding", "UTF-8");
+        if(header != null) {
+            header.forEach(http::setHeader);
+        }
         RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(35000).setConnectionRequestTimeout(35000).setSocketTimeout(60000).build();
-        httpGet.setConfig(requestConfig);
+        http.setConfig(requestConfig);
         CloseableHttpResponse httpResponse = null;
         try {
-            httpResponse = httpClient.execute(httpGet);
+            httpResponse = httpClient.execute(http);
             HttpEntity entity = httpResponse.getEntity();
             if (httpResponse.getStatusLine().getStatusCode() != 200) {
                 return null;
@@ -86,91 +83,6 @@ public class HttpRequestUtils {
                     httpResponse.close();
                 } catch (IOException e) {
                     // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-            if (null != httpClient) {
-                try {
-                    httpClient.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return null;
-    }
-
-    /**
-     * @Description: 发送http post请求
-     */
-    public static String doPost(String url, String jsonStr, Map<String, String> header) {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost(url);
-        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(35000).setConnectionRequestTimeout(35000).setSocketTimeout(60000).build();
-        header.forEach((k,v) -> httpPost.setHeader(k, v));
-        httpPost.setConfig(requestConfig);
-        CloseableHttpResponse httpResponse = null;
-        try {
-            httpPost.setEntity(new StringEntity(jsonStr));
-            httpResponse = httpClient.execute(httpPost);
-            if (httpResponse.getStatusLine().getStatusCode() != 200) {
-                return null;
-            }
-            HttpEntity entity = httpResponse.getEntity();
-            String result = EntityUtils.toString(entity);
-            return result;
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (httpResponse != null) {
-                try {
-                    httpResponse.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (null != httpClient) {
-                try {
-                    httpClient.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return null;
-    }
-
-    /**
-     * @Description: 发送http post请求
-     */
-    public static String doPost(String url, String jsonStr) {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost(url);
-        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(35000).setConnectionRequestTimeout(35000).setSocketTimeout(60000).build();
-        httpPost.setConfig(requestConfig);
-        httpPost.setHeader("Content-type", "application/json");
-        httpPost.setHeader("DataEncoding", "UTF-8");
-        CloseableHttpResponse httpResponse = null;
-        try {
-            httpPost.setEntity(new StringEntity(jsonStr));
-            httpResponse = httpClient.execute(httpPost);
-            if (httpResponse.getStatusLine().getStatusCode() != 200) {
-                return null;
-            }
-            HttpEntity entity = httpResponse.getEntity();
-            String result = EntityUtils.toString(entity);
-            return result;
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (httpResponse != null) {
-                try {
-                    httpResponse.close();
-                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
