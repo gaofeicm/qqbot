2dcbdcec255e017d4fd63491e5b31dbeaa6a5f43
Gaofeicm <gaofeicm@qq.com>
2022-08-23 14:34:46 +0800

    解决ck查询条件不正确的问题

--- src/main/java/com/gaofeicm/qqbot/dao/CookieDao.java
+++ src/main/java/com/gaofeicm/qqbot/dao/CookieDao.java
@@ -17,8 +17,8 @@ public interface CookieDao extends BaseMapper<Cookie> {
      * @return cks
      */
     @Select("SELECT c.priority, c.qq, c.cookie, q.name, q.address, q.toke_type as tokenType, q.token, q.client_id as clientId, q.client_secret as clientSecret FROM (SELECT * FROM cookie WHERE available = '1' and expiration_time > now()) c\n" +
-            "right join ql_cookie qc on (c.id = qc.cookie_id)\n" +
-            "left join ql q on (q.id = qc.ql_id)\n" +
-            "order by c.priority desc")
+            "join ql_cookie qc on (c.id = qc.cookie_id)\n" +
+            "join ql q on (q.id = qc.ql_id)\n" +
+            "order by c.priority desc,c.create_time")
     List<Map<String, Object>> getAvailableCookie();
 }
