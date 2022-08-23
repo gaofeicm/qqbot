b0e28d817fff06a6ba7834cb397c205df45eb9ae
Gaofeicm <gaofeicm@qq.com>
2022-08-23 14:37:59 +0800

    增加注释

--- src/main/java/com/gaofeicm/qqbot/QqbotApplication.java
+++ src/main/java/com/gaofeicm/qqbot/QqbotApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Gaofeicm
+ */
 @SpringBootApplication
 @MapperScan(basePackages = {"com.gaofeicm.qqbot.dao"})
 public class QqbotApplication {
