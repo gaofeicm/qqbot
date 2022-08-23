ca34fdc358876ea1647df7f2b7d2227f1943d1d7
Gaofeicm <gaofeicm@qq.com>
2022-08-23 14:37:51 +0800

    增加部分指令

--- src/main/java/com/gaofeicm/qqbot/event/message/FriendMessageEvent.java
+++ src/main/java/com/gaofeicm/qqbot/event/message/FriendMessageEvent.java
@@ -2,6 +2,7 @@ package com.gaofeicm.qqbot.event.message;
 
 import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
+import com.gaofeicm.qqbot.command.CommandService;
 import com.gaofeicm.qqbot.command.entity.Command;
 import com.gaofeicm.qqbot.entity.Cookie;
 import com.gaofeicm.qqbot.service.CookieService;
@@ -9,6 +10,8 @@ import com.gaofeicm.qqbot.service.JdServiceImpl;
 import com.gaofeicm.qqbot.service.QlService;
 import com.gaofeicm.qqbot.utils.*;
 import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -16,7 +19,10 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author Gaofeicm
@@ -24,8 +30,13 @@ import java.util.*;
 @Component
 public class FriendMessageEvent extends MessageEvent implements ApplicationRunner {
 
+    private final static Logger log = LoggerFactory.getLogger(FriendMessageEvent.class);
+
     @Resource
     private CookieService cookieService;
+
+    @Resource
+    private CommandService commandService;
 
     @Resource
     private QlService qlService;
@@ -43,8 +54,9 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
     @SneakyThrows
     @Override
     public void handle(JSONObject message) {
-        String msg = message.getString("message");
+        String msg = message.getString("message").trim();
         String qq = message.getString("user_id");
+        log.info("收到QQ：" + qq + "的消息，内容为:" + msg);
         //命令检查
         List<Command> cmds = CommandUtils.getCommand(qq);
         if(cmds != null && !cmds.isEmpty()){
@@ -53,11 +65,7 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
             int index = CommonUtils.indexOf(msg, cmd.getOption());
             if(index > -1){
                 JSONObject obj = (JSONObject) SpringUtils.invokeMethod(SpringUtils.getBean(cmd.getBean()), cmd.getAction().get(index).toString(), new Class[]{JSONObject.class}, cmd.getParam());
-                JSONArray messageList = obj.getJSONArray("message");
-                messageList.forEach(me -> {
-                    JSONObject m = (JSONObject) me;
-                    MessageUtils.sendPrivateMsg(m.getString("to"), m.getString("msg"));
-                });
+                this.executeReturnMessage(obj);
                 cmds.remove(cmd);
                 return;
             }else{
@@ -70,6 +78,7 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
                 sb.append("指令回复出错!您当前的待办任务如下：\r\n");
                 sb.append(cmd.getMessage());
                 MessageUtils.sendPrivateMsg(qq, sb.toString());
+                return;
             }
         }
         //ck检查
@@ -80,25 +89,48 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
             MessageUtils.sendPrivateMsg(qq, loadMsg);
             return;
         }
-        Map<String, String> header = new HashMap<>();
-        String s = "";
+        //按QQ查询信息
+        if(msg.startsWith("查询QQ：")){
+            if(!qq.equals(CommonUtils.getAdminQq())){
+                return;
+            }
+            String tq = msg.substring(5);
+            List<Cookie> cookies = cookieService.getCookieByQq(tq);
+            StringBuilder m = new StringBuilder("QQ").append(tq).append("共有").append(cookies.size()).append("个账号").append("\r\n");
+            if(cookies.size() > 0){
+                m.append("id  QQ                  pin                              启用    到期时间\r\n");
+                for (int i = 0; i < cookies.size(); i++) {
+                    Cookie cookie = cookies.get(i);
+                    m.append(cookie.getId()).append("      ").append(cookie.getQq()).append("    ").append(cookie.getPtPin(), 7, cookie.getPtPin().length() - 1).append("       ").append((boolean)cookie.getAvailable() ? "是" : "否").append("  ").append(SDF.format(cookie.getExpirationTime())).append("\r\n");
+                }
+            }
+            MessageUtils.sendPrivateMsg(qq, m.toString());
+            return;
+        }
         switch (msg){
+            case "查询":
+                this.findCkAccount(qq);
+                break;
+            case "菜单":
+                MessageUtils.sendPrivateMsg(qq, this.getMenuStr());
+                break;
             case "同步":
                 this.syncQlCookie(qq, msg);
                 break;
-            case "查询青龙":
-                header.put("Authorization", "Bearer 558b2e8b-f6de-49c8-b91b-0a21f8c1f60d");
-                header.put("User-Agent", "qqbot client");
-                header.put("content-type", "application/json");
-                s = HttpRequestUtils.doGet("http://wou.gaofeicm.cf:8080/open/envs?searchValue=&t=" + System.currentTimeMillis(), header);
-                MessageUtils.sendPrivateMsg(qq, s);
+            case "设置过期时间":
+                this.executeReturnMessage(commandService.setCookieExpStr(new JSONObject(){{put("to", qq);}}));
                 break;
-            case "查询":
-                List<Cookie> cookies = cookieService.getCookieByQq(qq);
-                cookies.forEach(cookie -> {
-                    String ckAccount = this.findCkAccount(cookie.getCookie());
-                    MessageUtils.sendPrivateMsg(qq, ckAccount);
-                });
+            case "分配面板":
+                this.executeReturnMessage(commandService.addQlCookieStr(new JSONObject(){{put("to", qq);}}));
+                break;
+            case "检查":
+                jdService.cronCheckCkLogin();
+                break;
+            case "启动检查":
+                JdServiceImpl.checkCkSwitch = true;
+                break;
+            case "关闭检查":
+                JdServiceImpl.checkCkSwitch = false;
                 break;
             case "原理":
                 MessageUtils.sendPrivateMsg(qq, "1、自动帮忙做一些需要手动点的任务\r\n 2、一些活动需要邀请好友帮忙做任务，大家挂一起都是互相互助\r\n");
@@ -122,12 +154,16 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
             case "二次元":
                 MessageUtils.sendPrivateMsg(qq, "[CQ:image,file=http://www.dmoe.cc/random.php,cache=0]");
                 break;
+            case "打赏":
+                MessageUtils.sendPrivateMsg(qq, "[CQ:image,file=https://raw.githubusercontent.com/gaofeicm/gaofeicm.github.io/master/assets/images/donate.png,cache=1]");
+                break;
             case "新闻":
-                MessageUtils.sendPrivateMsg(qq, "今日新闻（" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "）：\r\n" + JSONObject.parseObject(HttpRequestUtils.doGet("http://bjb.yunwj.top/php/qq.php")).getString("wb").replaceFirst("【换行】", "").replaceAll("【换行】", "\r\n"));
+                MessageUtils.sendPrivateMsg(qq, "今日新闻：\r\n" + JSONObject.parseObject(HttpRequestUtils.doGet("http://bjb.yunwj.top/php/qq.php")).getString("wb").replaceFirst("【换行】", "").replaceAll("【换行】", "\r\n"));
                 break;
             default:
-                MessageUtils.sendPrivateMsg(qq, "我只是个笨蛋机器人，你说的话我还不会接。\r\n目前打算上线签到系统，连续签到可以赢取代挂时长哦！");
+                MessageUtils.sendPrivateMsg(qq, "我只是个笨蛋机器人，你说的话我还不会接。不要连续发我听不懂的话，发多了我不理你了！");
         }
+
     }
 
     @Override
@@ -213,7 +249,7 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
     }
 
     /**
-     * 添加ck
+     * 添加面板原始数据
      * @param map map
      * @param list list
      */
@@ -221,14 +257,14 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
         try{
             Map<String, String> header = this.getQlHeader(map);
             String body = list.toJSONString();
-            String s = HttpRequestUtils.doPost("http://wou.gaofeicm.cf:8080//open/envs?t=" + System.currentTimeMillis(), body, header);
+            String s = HttpRequestUtils.doPost(map.get("address") + "/open/envs?t=" + System.currentTimeMillis(), body, header);
             if (s != null) {
                 JSONObject jsonObject = JSONObject.parseObject(s);
                 if (!"200".equals(jsonObject.getString("code"))) {
-                    throw new RuntimeException();
+                    throw new RuntimeException("code为" + jsonObject.getString("code"));
                 }
             }else{
-                throw new RuntimeException("返回值为null");
+                throw new RuntimeException("添加面板原始数据返回值为null");
             }
         }catch (Exception e){
             MessageUtils.sendPrivateMsg(CommonUtils.getAdminQq(), "添加原始数据出现了异常！" + map.toString() + e.getMessage());
@@ -257,7 +293,7 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
                         throw new RuntimeException();
                     }
                 }else{
-                    throw new RuntimeException("返回值为null");
+                    throw new RuntimeException("删除面板原始数据返回值为null");
                 }
             }
         }catch (Exception e){
@@ -283,12 +319,47 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
     }
 
     /**
+     * 获取用户信息
+     * @param qq qq
+     */
+    private void findCkAccount(String qq){
+        List<Cookie> cookies = cookieService.getCookieByQq(qq);
+        int count = cookies.size();
+        if(count == 0){
+            MessageUtils.sendPrivateMsg(qq, "您还没有添加账号，请根据群公告获取ck后发给我");
+            return;
+        }
+        long t = System.currentTimeMillis();
+        List<Cookie> availableCookies = cookies.stream().filter(cookie -> (boolean) cookie.getAvailable() && cookie.getExpirationTime().getTime() > t).collect(Collectors.toList());
+        long count1 = availableCookies.size();
+        long count2 = cookies.stream().filter(cookie -> !(boolean) cookie.getAvailable() || cookie.getExpirationTime().getTime() <= t).count();
+        StringBuilder m = new StringBuilder("执行查询命令，您总共有").append(cookies.size()).append("个账号");
+        if(count1 > 0 && count2 > 0){
+            m.append("，").append(count1).append("个有效账号，").append(count2).append("个失效账号！\r\n");
+            m.append("可能存在的情况：\r\n1、代挂服务未过期，ck过期了，请重新提交，并联系管理员刷新ck\r\n2、代挂服务过期了，请续费后重新获取ck发送给我，并联系管理员审核\r\n\r\n");
+        }
+        if(count1 == 0){
+            m.append("，").append(count2).append("个失效账号！\r\n");
+        }
+        MessageUtils.sendPrivateMsg(qq, m.toString());
+        if(count1 > 0){
+            availableCookies.forEach(cookie -> {
+                String ckAccount = this.findCkAccountInfo(cookie.getCookie());
+                MessageUtils.sendPrivateMsg(qq, ckAccount);
+            });
+        }
+    }
+
+    /**
      * 查询账户信息
      * @param  ck ck
      * @return 查询结果
      */
-    private String findCkAccount(String ck){
+    private String findCkAccountInfo(String ck){
         JSONObject userInfo1 = jdService.getUserInfo1(ck);
+        if(userInfo1 != null && "1001".equals(userInfo1.getString("retcode"))){
+            return "您的ck:[" + ck + "]已过期！";
+        }
         JSONObject userInfo2 = jdService.getUserInfo2(ck);
         Map<String, Object> bean = jdService.getBean(ck);
         JSONObject jxmc = jdService.getJxmc(ck);
@@ -297,9 +368,6 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
         JSONObject jdms = jdService.getJdms(ck);
         JSONObject jdww = jdService.getJdww(ck);
         JSONObject jdnc = jdService.getJdnc(ck);
-        if("1001".equals(userInfo1.getString("retcode"))){
-            return "您的ck:[" + ck + "]已过期！";
-        }
         StringBuffer msg = new StringBuffer("【账号\uD83C\uDD94】");
         String nickName = "";
         String levelName = "";
@@ -308,7 +376,7 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
         int beanCount = 0;
         String couponNum = "";
         String jingXiang = "";
-        if("0".equals(userInfo1.getString("retcode"))){
+        if(userInfo1 != null && "0".equals(userInfo1.getString("retcode"))){
             JSONObject data = userInfo1.getJSONObject("data");
             if(data != null && data.containsKey("userInfo")){
                 JSONObject userInfo = data.getJSONObject("userInfo");
@@ -333,23 +401,19 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
         msg.append("【昨日京豆】").append(bean.get("yesterday")).append("\r\n");
         msg.append("【当前京豆】").append(beanCount).append("豆(≈").append(new DecimalFormat("0.00").format((float)beanCount / 100)).append("元)\r\n");
         msg.append("【优惠券】").append(couponNum).append("张\r\n");
-        if("0".equals(jxmc.getString("ret"))){
-            int v = jxmc.getJSONObject("data").getIntValue("eggcnt");
-            msg.append("【京喜牧场】").append(v).append("枚鸡蛋\r\n");
-        }
-        if("0".equals(jdjs.getString("code"))){
+        if(jdjs != null && "0".equals(jdjs.getString("code"))){
             int v = jdjs.getJSONObject("data").getIntValue("goldBalance");
             msg.append("【极速金币】").append(v).append("币(≈").append(new DecimalFormat("0.00").format((float)v / 10000)).append("元)\r\n");
         }
-        if("0".equals(jdzz.getString("code"))){
+        if(jdzz != null && "0".equals(jdzz.getString("code"))){
             int v = jdzz.getJSONObject("data").getIntValue("totalNum");
             msg.append("【京东赚赚】").append(v).append("币(≈").append(new DecimalFormat("0.00").format((float)v / 10000)).append("元)\r\n");
         }
-        if("2041".equals(jdms.getString("code")) || "2042".equals(jdms.getString("code"))){
+        if(jdms != null && "2041".equals(jdms.getString("code")) || "2042".equals(jdms.getString("code"))){
             int v = jdms.getJSONObject("result").getJSONObject("assignment").getIntValue("assignmentPoints ");
             msg.append("【京东秒杀】").append(v).append("币(≈").append(new DecimalFormat("0.00").format((float)v / 1000)).append("元)\r\n");
         }
-        if("0".equals(jxmc.getString("ret"))){
+        if(jxmc != null && "0".equals(jxmc.getString("ret"))){
             int v = jxmc.getJSONObject("data").getIntValue("eggcnt");
             if(v == 0){
                 msg.append("【京喜牧场】未开通或提示火爆\n");
@@ -358,14 +422,14 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
                 msg.append("【京喜牧场】").append(v).append("枚鸡蛋\r\n");
             }
         }
-        if(jdww.getBooleanValue("success")){
+        if(jdww != null && jdww.getBooleanValue("success")){
             int v = jdww.getIntValue("level");
             msg.append("【汪汪乐园】汪汪等级:").append(v).append("级\r\n");
         }else{
             String v = jdww.getString("errMsg");
             msg.append("【汪汪乐园】").append(v).append("\r\n");
         }
-        if(jdnc.getString("JdFarmProdName") != null){
+        if(jdnc != null && jdnc.getString("JdFarmProdName") != null){
             msg.append("【东东农场】");
             int treeState = jdnc.getIntValue("treeState");
             if(jdnc.getIntValue("JdtreeEnergy") != 0){
@@ -418,12 +482,42 @@ public class FriendMessageEvent extends MessageEvent implements ApplicationRunne
         return sb.toString();
     }
 
+    /**
+     * 获取菜单回复文字
+     * @return 教程
+     */
+    private String getMenuStr(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("------------京 东 区------------\r\n");
+        sb.append("查询   原理    教程    签到（未上线）\r\n");
+        sb.append("------------娱 乐 区------------\r\n");
+        sb.append("打赏 | 你的鼓励是我最大的动力\r\n");
+        sb.append("小姐姐 | 看小姐姐\r\n");
+        sb.append("二次元 | 看二次元小姐姐\r\n");
+        sb.append("舔狗 | 获取舔狗日记\r\n");
+        sb.append("舔狗日记 | 获取舔狗日记\r\n");
+        sb.append("新闻 | 获取时事新闻\r\n");
+        return sb.toString();
+    }
+
     public Map<String, String> getQlHeader(Map<String, Object> map){
         Map<String, String> header = new HashMap<>();
         header.put("Authorization", map.get("tokenType") + " " + map.get("token"));
         header.put("User-Agent", "qqbot client");
         header.put("content-type", "application/json");
         return header;
+    }
+
+    /**
+     * 处理命令返回待发送的信息
+     * @param obj 信息集合
+     */
+    private void executeReturnMessage(JSONObject obj){
+        JSONArray messageList = obj.getJSONArray("message");
+        messageList.forEach(me -> {
+            JSONObject m = (JSONObject) me;
+            MessageUtils.sendPrivateMsg(m.getString("to"), m.getString("msg"));
+        });
     }
 
 }
